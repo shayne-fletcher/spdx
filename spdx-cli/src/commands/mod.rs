a8f6966b9bad0411bd065371bd4af37943ef8b04
Shayne Fletcher <shaynefletcher@meta.com>
2023-10-25 14:05:29 -0400

    list Cargo.toml files

--- spdx-cli/src/commands/mod.rs
+++ spdx-cli/src/commands/mod.rs
@@ -1,5 +1,7 @@
+pub mod list;
 pub mod name;
 pub mod repl;
 
+pub use list::list;
 pub use name::name;
 pub use repl::repl;
--- spdx-cli/src/main.rs
+++ spdx-cli/src/main.rs
@@ -1,27 +1,37 @@
 mod commands;
 mod init;
 
-use clap::{Parser, Subcommand};
-use std::error::Error;
-use std::sync::OnceLock;
-
 use spdx_lib::{LicenseExceptionList, LicenseId, LicenseList};
+
+use clap::{Parser, Subcommand};
+
+use std::error::Error;
+use std::path::PathBuf;
+use std::sync::OnceLock;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    #[arg(long)]
+    num_threads: Option<usize>,
+
     #[command(subcommand)]
     command: Option<Commands>,
 }
 
-// cargo run -p spdx-cli -- repl
-// cargo run -p spdx-cli -- name --license-id "AGPL-3.0"
 #[derive(Subcommand)]
 enum Commands {
+    // cargo run -p spdx-cli -- repl
     Repl,
+    // cargo run -p spdx-cli -- name --license-id "AGPL-3.0"
     Name {
         #[arg(short, long, value_parser = clap::value_parser!(LicenseId))]
         license_id: LicenseId,
+    },
+    // cargo run -p spdx-cli -- list --root "$(pwd)"
+    List {
+        #[arg(short, long)]
+        root: PathBuf,
     },
 }
 
@@ -31,11 +41,19 @@ static LICENSE_EXCEPTIONS: OnceLock<LicenseExceptionList> = OnceLock::new();
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
+    if let Some(num_threads) = args.num_threads {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(num_threads)
+            .build_global()
+            .unwrap();
+    }
+
     init::init_license_data()?;
 
     match &args.command {
         Some(Commands::Repl) => commands::repl(),
         Some(Commands::Name { license_id }) => commands::name(license_id),
+        Some(Commands::List { root }) => commands::list(root),
         None => (),
     }
 
